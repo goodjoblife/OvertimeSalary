0f7be790b165c1efb309ab30c7425201114bca95
Yen-chi Chen <mark86092@gmail.com>
2016-06-18 14:09:21 +0800

    show modal on load

--- js/main.js
+++ js/main.js
@@ -7,6 +7,8 @@
      * A place to prepare our checker
      */
     var init = global.init = function() {
-    
+        $('#test-modal').modal('show');
     };
 })(window, jQuery);
+
+//window.init();
