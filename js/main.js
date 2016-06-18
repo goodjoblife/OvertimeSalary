7c36c5948730b819f0e432754baedf24cea7c9bc
Qing-Cheng Li <qcli@yahoo-inc.com>
2016-06-18 14:41:31 +0800

    Bring in default work time info

--- js/main.js
+++ js/main.js
@@ -4,11 +4,31 @@
     };
 
     /*
+     * Event listeners in modal view
+     */
+
+    var initWorktimeModalView = function(worktime) {
+        console.log("Init modal view");
+        console.log(worktime);
+    };
+
+    $('#test-modal').on('shown.bs.modal', function() {
+        var defaultWorktime = {
+            'startTime':'08:00:00',
+            'endTime':'17:00:00',
+            'freeTime': '1'
+        };
+
+        initWorktimeModalView(defaultWorktime);
+    });
+
+    /*
      * A place to prepare our checker
      */
     var init = global.init = function() {
         $('#test-modal').modal('show');
     };
+
 })(window, jQuery);
 
-//window.init();
+// window.init();
