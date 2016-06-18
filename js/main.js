5e561067da5d9bcb8960b3e754dc33145f27c749
Yen-chi Chen <mark86092@gmail.com>
2016-06-18 20:53:44 +0800

    separate the logics

--- js/main.js
+++ js/main.js
@@ -3,16 +3,28 @@
 
     };
 
-    /*
-     * Event listeners in modal view
-     */
+    // sharing the data with modal and table
+    // make sure the event is bind via bindShownEvent, bindHiddenEvent
+    var shared = undefined;
+    function bindShownEvent(shown) {
+        $('#test-modal').on('shown.bs.modal', function () {
+            var data = shared;
+            shown(data);
+        });
+    }
+    function bindHiddenEvent(hidden) {
+        $('#test-modal').on('hidden.bs.modal', function () {
+            var data = shared;
+            hidden(data);
+        });
+    }
 
-    Number.prototype.toHM = function () {
+    Number.prototype.toHM = function() {
         return Math.floor(this/60) + "時" + this%60 + "分";
     }
 
-    var initWorktimeModalView = function(worktime) {
-        // selectors
+    // init the Modal, run once
+    function initModal() {
         var $st = $('#wtm-startTime');
         var $et = $('#wtm-endTime');
         var $ft = $('#wtm-freeTime');
@@ -20,61 +32,51 @@
         var $rt = $('#wtm-releaseTime');
         var $tt = $('#wtm-totalTime');
 
-        var changeResult = function() {
+        $st.change(function () {
+            changeResult();
+        });
+        $et.change(function () {
+            changeResult();
+        });
+        $ft.change(function () {
+            changeResult();
+        });
+        
+        function changeResult() {
             console.log("Need re calculate");
             var ft = parseInt($ft.val());
-
             var stm = moment($st.val(), "hh:mm");
             var etm = moment($et.val(), "hh:mm");
-
             var wtdiff = etm.diff(stm, "minutes");
 
             // FIXME
-            $wt.html(Math.floor(wtdiff/60) + "時" + wtdiff%60 + "分");
+            $wt.html(wtdiff.toHM());
             $rt.html(ft + "時");
             var totalTimeInMinutes = wtdiff - ft * 60;
-            //$tt.html(Math.floor(totalTimeInMinutes/60) + "時" + totalTimeInMinutes%60 + "分");
             $tt.html(totalTimeInMinutes.toHM());
         };
-        $st.val(worktime.startTime).change(function(){
-            changeResult();
-        });
-        $et.val(worktime.endTime).change(function(){
-            changeResult();
-        });
-        $ft.val(worktime.freeTime).change(function(){
-            changeResult();
-        });
 
-        changeResult();
-    };
-
-    $('#test-modal').on('shown.bs.modal', function(event) {
-        var data = event.relatedTarget;
-
-        console.log(data);
-
-        initWorktimeModalView(data);
-        
         $("#test-modal-button").on('click', function () {
-            $("#test-modal-button").off('click');
-            
-            $('#test-modal').on('hidden.bs.modal', function (event) {
-                $('#test-modal').off('hidden.bs.modal');
-
-                data.obj.trigger('gj.changed');
-            });
-
             $('#test-modal').modal('hide');
         });
-        
-    });
 
-    var workTimes = [];
-    /*
-     * A place to prepare our checker
-     */
-    var init = global.init = function() {
+        bindShownEvent(function (data) {
+            var worktime = data;
+
+            // initial all value
+            $st.val(worktime.startTime);
+            $et.val(worktime.endTime);
+            $ft.val(worktime.freeTime);
+
+            changeResult();
+        });
+    }
+   
+    // init the Table, run once
+    function initTable() {
+        bindHiddenEvent(function(data) {
+            data.obj.trigger('view.update');
+        });
 
         [1, 2, 3, 4, 5, 6, 7].forEach(function(weekDay) {
             var me = $("#weekday-" + weekDay);
@@ -86,27 +88,34 @@
                 freeTime: '1',
             };
 
-            workTimes.push(data);
-
-            me.on('click', function(e) {
-                $('#test-modal').modal('show', data);
+            me.on('click', function () {
+                // update the shared data
+                shared = data;
+                $('#test-modal').modal('show');
             });
         
-            me.on('gj.changed', function() {
-                console.log("gj.changed");
-
+            me.on('view.update', function() {
                 me.find(".start").html(data.startTime);
                 me.find(".end").text(data.endTime);
                 me.find(".break").text(data.freeTime);
             });
 
-            me.trigger('gj.changed');
+            me.trigger('view.update');
         });
+    }
 
-        $("#weekday-1").click();
+    /*
+     * A place to prepare our checker
+     */
+    var init = global.init = function(callback) {
+        initTable();
+        initModal();
 
+        callback && callback();
     };
 
 })(window, jQuery);
 
-window.init();
+window.init(function () {
+    $("#weekday-1").click();
+});
