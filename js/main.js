50e891429241a833865d47db57045108e6bdf69d
Qing-Cheng Li <qcli@yahoo-inc.com>
2016-06-18 16:04:19 +0800

    Automatically calculation

--- js/main.js
+++ js/main.js
@@ -8,14 +8,46 @@
      */
 
     var initWorktimeModalView = function(worktime) {
-        console.log("Init modal view");
-        console.log(worktime);
+        // selectors
+        var $st = $('#wtm-startTime');
+        var $et = $('#wtm-endTime');
+        var $ft = $('#wtm-freeTime');
+        var $wt = $('#wtm-workTime');
+        var $rt = $('#wtm-releaseTime');
+        var $tt = $('#wtm-totalTime');
+
+        var changeResult = function() {
+            console.log("Need re calculate");
+            var ft = parseInt($ft.val());
+
+            var stm = moment($st.val(), "hh:mm");
+            var etm = moment($et.val(), "hh:mm");
+
+            var wtdiff = etm.diff(stm, "minutes");
+
+            // FIXME
+            $wt.html(wtdiff/60 + "時" + wtdiff%60 + "分");
+            $rt.html(ft + "時");
+            var totalTimeInMinutes = wtdiff - ft * 60;
+            $tt.html(totalTimeInMinutes/60 + "時" + totalTimeInMinutes%60 + "分");
+        };
+        $st.val(worktime.startTime).change(function(){
+            changeResult();
+        });
+        $et.val(worktime.endTime).change(function(){
+            changeResult();
+        });
+        $ft.val(worktime.freeTime).change(function(){
+            changeResult();
+        });
+
+        changeResult();
     };
 
     $('#test-modal').on('shown.bs.modal', function() {
         var defaultWorktime = {
-            'startTime':'08:00:00',
-            'endTime':'17:00:00',
+            'startTime':'08:00',
+            'endTime':'17:00',
             'freeTime': '1'
         };
 
