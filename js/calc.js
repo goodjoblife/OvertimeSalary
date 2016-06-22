369cdf39f9693f64c6bae54b979a7ae5e7d741a2
Wei-Ming <barry800414@gmail.com>
2016-06-22 10:54:40 +0800

    fix bugs & add test case

--- js/calc.js
+++ js/calc.js
@@ -9,27 +9,6 @@ if (typeof module !== 'undefined') {
     m = moment;
 }
 (function (exports, moment) {
-/*
-
-Definition of names:
-工時(WorkingTime):
-	->正常工時(NormalWorkingTime, nwt):
-		->非休假日: 平日每小時工資
-		->休假日(NormalWorkingTimeOnNormalDay, nwtNormal): 
-			->例假日(NormalWorkingTimeOnRoutineDayOff, nwtRoutineOff): 外加8小時薪資，需補休  //TODO
-			->國定休假日: 
-				->輪班人員 & 調移至其他日休假: 平日每小時工資(暫不考慮)
-				->否則(NormalWorkingTimeOnNationalHoliday, nwtHoliday): 外加8小時薪資，不需補休。  //TODO
-			->特休日: 暫不考慮
-	->延長工時(ExtendedWorkingTime, ewt)
-		->超過每日8小時: 
-			->休假日(ExtendedWorkingTimeOnDayOff, ewtDayOff): 1.33開始 
-			->正常工作日(ExtendedWorkingTimeOnNormalDay, ewtNormal): 1.33開始 
-			->休息日(以上兩者除外) (ExtendedWorkingTimeOnRestDay_Exceed8Hour, ewtRestMore): 1.66開始 *1
-		->未超過每日8小時，但超過一週40小時(ExtendedWorkingTimeOnRestDay, ewtRest) : 0.33開始 
-
-*/
-
 
 //rule 30.1 
 const normalDayTime = moment.duration(8, 'hours');
@@ -56,6 +35,10 @@ function calcWorkingTime(startTime, endTime, breakDuration){
 exports.calcWorkingTime = calcWorkingTime;
 
 /*
+ * @param  startTime     moment
+ * @param  endTime       moment
+ * @param  breakDuration Duration
+ * @param  isRoutineDayOff boolean
 	->正常工時(NormalWorkingTime, nwt):
 		->非休假日: 平日每小時工資
 		->休假日(NormalWorkingTimeOnNormalDay, nwtNormal): 
@@ -93,28 +76,38 @@ function divideWorkingTime(startTime, endTime, breakDuration, isRoutineDayOff=fa
 }
 
 function __calcNormalWorkingTime(workingTime){
-	return (workingTime > normalDayTime) ? normalDayTime : workingTime ; 
+	return (workingTime > normalDayTime) ? moment.duration(normalDayTime) : workingTime ; 
 }
 function __calcExtendedWorkingTime(workingTime){
-	return (workingTime > normalDayTime) ? (workingTime - normalDayTime) : moment.duration(0) ; 
+	return (workingTime > normalDayTime) ? moment.duration(workingTime - normalDayTime) : moment.duration(0) ; 
 }
 
+/*
+ * @param  startTimeArr     array of moment
+ * @param  endTimeArr       array of moment
+ * @param  breakDurationArr array of Duration
+ * @param  routineDayOffDay number(from 0 to 6)
+ * @param  restDay          number(from 0 to 6)
+*/
 function divideWorkingTime_OneWeek(startTimeArr, endTimeArr, breakDurationArr, routineDayOffDay=0, restDay=6){
 	const nDays = 7;
-	var nwtNormalArr = new Array[nDays];
-	var nwtRoutineOffArr = new Array[nDays];
-	var nwtHolidayArr = new Array[nDays];
-	var ewtNormalArr = new Array[nDays];
-	var ewtDayOffArr = new Array[nDays];
-	var ewtRestArr = new Array[nDays];
-	var ewtRestMoreArr = new Array[nDays];
+	var nwtNormalArr = new Array();
+	var nwtRoutineOffArr = new Array();
+	var nwtHolidayArr = new Array();
+	var ewtNormalArr = new Array();
+	var ewtDayOffArr = new Array();
+	var ewtRestArr = new Array();
+	var ewtRestMoreArr = new Array();
 	var ntwWeek = moment.duration(0);
 	
 	//get normal working time for each day
 	for(var i = 0; i < nDays; i++){
 		var isRoutineDayOff = __checkRoutineDayOff(startTimeArr[i], routineDayOffDay);
-		var isNationalHoliday = isNationalHoliday(startTimeArr[i]);
-		[nwtNormalArr[i], nwtRoutineOffArr[i], nwtHolidayArr[i], ewtNormalArr, ewtDayOffArr] = divideWorkingTime(startTimeArr[i], endTimeArr[i], breakDurationArr[i], isRoutineDayOff);
+		[nwtNormalArr[i], nwtRoutineOffArr[i], nwtHolidayArr[i], ewtNormalArr[i], 
+			ewtDayOffArr[i]] = divideWorkingTime(startTimeArr[i], endTimeArr[i], breakDurationArr[i], isRoutineDayOff);
+
+		ewtRestArr[i] = moment.duration(0);
+		ewtRestMoreArr[i] = moment.duration(0);
 		ntwWeek.add(nwtNormalArr[i]);		
 	}
 	/*if sum of normal working time for is longer than normal working time per week, distribute those
@@ -141,19 +134,22 @@ function divideWorkingTime_OneWeek(startTimeArr, endTimeArr, breakDurationArr, r
 }
 
 function __calcExtendedWeekWorkingTime(workingTime){
-	return (workingTime > normalWeekTime) ? (workingTime - normalWeekTime) : moment.duration(0) ; 
+	return (workingTime > normalWeekTime) ? moment.duration(workingTime - normalWeekTime) : moment.duration(0) ; 
 }
 
 function __checkRoutineDayOff(date, routineDayOffDay){
+	if(date === null) { return false; }
 	return date.day() == routineDayOffDay;
 }
 
 function __getDayMapping(dateArr){
 	var nDays = dateArr.length;
-	var mapping = new Array[nDays];
+	var mapping = new Array();
 	for(var i=0; i < nDays; i++){
-		var day = dateArr[i].day();
-		mapping[day] = i;
+		if(dateArr[i] !== null){
+			var day = dateArr[i].day();
+			mapping[day] = i;	
+		}
 	}
 	return mapping;
 }
@@ -162,44 +158,68 @@ function __getDayMapping(dateArr){
 function isNationalHoliday(date){
 	return false;
 }
+exports.isNationalHoliday = isNationalHoliday;
 
 
-function calcOvertimeSalary(nwtRoutineOff, nwtHoliday, ewtNormal, ewtDayOff, ewtRest, ewtRestMore, hourSalary, bonus=[4.0/3.0, 5.0/3.0, 5.0/3.0]){
+function calcOvertimeSalary(nwtRoutineOff, nwtHoliday, ewtNormal, ewtDayOff, 
+	ewtRest, ewtRestMore, hourSalary, salaryBonus=[4.0/3.0, 5.0/3.0, 5.0/3.0]){
 	var salary = 0.0;
+	var h = 0; 
 	if(nwtRoutineOff != 0 || nwtHoliday != 0){
 		salary += normalDayTime.hours() * hourSalary;
 	}
 
 	if(ewtNormal != 0 || ewtDayOff != 0){ //one of them should be 0
-		var h = (ewtNormal + ewtDayOff).hours(); 
-		var base = 2;
-		for(var i = 0; i < bonus.length - 1; i++){
-			if(h > base){
-				salary += base * hourSalary * bonus[i];
-				h -= base; 
-			}
-			else{
-				salary += h * hourSalary * bonus[i];
-				h = 0;
-				break;
-			}
-		}
-		if(h > 0){
-			salary += h * hourSalary * bonus[bonus.length -1];
-		}
+		h = ewtNormal.hours();
+	}
+	else if(ewtDayOff != 0){
+		h = ewtDayOff.hours();
 	}
 	else if(ewtRest != 0 || ewtRestMore != 0){
-
+		h = ewtRest.hours() + ewtRestMore.hours();
 	}
 
+	//calculate overtime salary 
+	var base = 2;
+	for(var i = 0; i < salaryBonus.length - 1; i++){
+		if(h > base){
+			salary += base * hourSalary * salaryBonus[i];
+			h -= base; 
+		}
+		else{
+			salary += h * hourSalary * salaryBonus[i];
+			h = 0;
+			break;
+		}
+	}
+	if(h > 0){
+		salary += h * hourSalary * salaryBonus[salaryBonus.length - 1];
+	}
+	
+	//substract the money already given
+	if(ewtRest != 0){
+		salary -= ewtRest.hours() * hourSalary; 
+	}
+	return salary;
 }
 
-function calcOvertimeSalary_OneWeek(startTimeArr, endTimeArr, breakDurationArr, routineDayOffDay=0, restDay=6){
+function calcOvertimeSalary_OneWeek(startTimeArr, endTimeArr, breakDurationArr, hourSalary, 
+	routineDayOffDay=0, restDay=6, salaryBonus=[4.0/3.0, 5.0/3.0, 5.0/3.0]){
 	[nwtNormalArr, nwtRoutineOffArr, nwtHolidayArr, ewtNormalArr, ewtDayOffArr, 
 		ewtRestArr, ewtRestMoreArr] = divideWorkingTime_OneWeek(startTimeArr, endTimeArr, 
 			breakDurationArr, routineDayOffDay, restDay);
 
+	const nDays = 7;		
+	var overtimeSalary = 0.0;
+	for(var i = 0; i < nDays; i++){
+		overtimeSalary += calcOvertimeSalary(nwtRoutineOffArr[i], nwtHolidayArr[i], 
+			ewtNormalArr[i], ewtDayOffArr[i], ewtRestArr[i], ewtRestMoreArr[i], hourSalary, salaryBonus);
+	}
+	return overtimeSalary;
 }
+exports.calcOvertimeSalary_OneWeek = calcOvertimeSalary_OneWeek;
+
 
 })(e, m);
 })();
+
