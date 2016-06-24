f6b596a8c3d88d6bf7dcadef4931f35e7ac7557b
Yen-chi Chen <mark86092@gmail.com>
2016-06-24 18:10:49 +0800

    refactor

--- js/main.js
+++ js/main.js
@@ -1,34 +1,40 @@
 (function(exports, $, moment, calc) {
-    var workingTimes = exports.workingTimes = [];
+    var workingsArr = exports.workingsArr = [];
+
     exports.monthSalary = 0;
 
-    for (var i = 0; i < 7; i++) {
-        work = new calc.Working();
-        work.startTime = moment().weekday(i - 7).hour(8).minute(0).second(0).millisecond(0);
-        work.endTime   = moment().weekday(i - 7).hour(17).minute(0).second(0).millisecond(0);
-        work.freeTime  = moment.duration(1, "hours");
-        work.isBreak   = false;
-        work.isRoutineDayOff = false;
+    function initData() {
+        for (var i = 0; i < 7; i++) {
+            working = new calc.Working();
+            working.startTime       = moment().weekday(i - 7).hour(8).minute(0).second(0).millisecond(0);
+            working.endTime         = moment().weekday(i - 7).hour(17).minute(0).second(0).millisecond(0);
+            working.breakDuration   = moment.duration(1, "hours");
+            working.isBreak         = false;
+            working.isRoutineDayOff = false;
 
-        workingTimes.push(work);
+            workingsArr.push(working);
+        }
+        workingsArr[0].isRoutineDayOff = true;
+        workingsArr[0].isBreak = true;
     }
-    workingTimes[0].isRoutineDayOff = true;
 
-    var callbacks = {};
-    exports.on = function(type, callback) {
-        if (!(type in callbacks)) {
-            callbacks[type] = [];
+    (function() {
+        var callbacks = {};
+        exports.on = function(type, callback) {
+            if (!(type in callbacks)) {
+                callbacks[type] = [];
+            }
+            callbacks[type].push(callback);
         }
-        callbacks[type].push(callback);
-    }
-    exports.emit = function(type, data) {
-        if (!(type in callbacks)) {
-            return;
+        exports.emit = function(type, data) {
+            if (!(type in callbacks)) {
+                return;
+            }
+            callbacks[type].forEach(function(callback) {
+                callback.call(exports, data);
+            });
         }
-        callbacks[type].forEach(function(callback) {
-            callback.call(exports, data);
-        });
-    }
+    })();
 
     Number.prototype.toHM = function() {
         return Math.floor(this/60) + "時" + this%60 + "分";
@@ -38,14 +44,14 @@
     function initModal() {
         var $st = $('#wtm-startTime');
         var $et = $('#wtm-endTime');
-        var $ft = $('#wtm-freeTime');
+        var $bd = $('#wtm-breakDuration');
         var $ib = $('#wtm-isBreak');
         var $irdo = $('#wtm-isRoutineDayOff');
         var $wt = $('#wtm-workTime');
         var $rt = $('#wtm-releaseTime');
         var $tt = $('#wtm-totalTime');
 
-        var weekDay = null;
+        var pointer = null;
 
         $st.change(function () {
             changeResult();
@@ -53,119 +59,126 @@
         $et.change(function () {
             changeResult();
         });
-        $ft.change(function () {
+        $bd.change(function () {
             changeResult();
         });
         $ib.change(function() {
             changeResult();
         });
 
-        function formToData() {
-            var data = {
-                freeTime        : moment.duration(parseInt($ft.val()), "minutes"),
-                startTime       : moment($st.val(), "HH:mm"),
-                endTime         : moment($et.val(), "HH:mm"),
-                isBreak         : ($ib.prop("checked") === true),
-                isRoutineDayOff : ($irdo.prop("checked") === true),
-            }
-            return data;
+        function formToWork(work) {
+            work.breakDuration   = moment.duration(parseInt($bd.val()), "minutes");
+            work.startTime.hour($st.val().split(":")[0]).minute($st.val().split(":")[1]);
+            work.endTime.hour($et.val().split(":")[0]).minute($et.val().split(":")[1]);
+            work.isBreak         = ($ib.prop("checked") === true);
+            work.isRoutineDayOff = ($irdo.prop("checked") === true);
+
+            return work;
         }
 
         function changeResult() {
-            console.log("Need re calculate");
-            
-            var workingTime = formToData();
-            var workingTimeDuration = moment.duration(workingTime.endTime -workingTime.startTime);
-            var totalTimeDuration   = moment.duration(workingTimeDuration - workingTime.freeTime);
+            var work = workingsArr[pointer].clone();
+            formToWork(work);
+            var workingTimeDuration = moment.duration(work.endTime - work.startTime);
+            var totalTimeDuration   = moment.duration(workingTimeDuration - work.breakDuration);
 
-            if (workingTime.isBreak) {
+            if (work.isBreak) {
                 $wt.html((0).toHM());
                 $rt.html((0).toHM());
                 $tt.html((0).toHM());
 
                 $st.prop("disabled", true);
                 $et.prop("disabled", true);
-                $ft.prop("disabled", true);
+                $bd.prop("disabled", true);
             } else {
                 $wt.html(workingTimeDuration.asMinutes().toHM());
-                $rt.html(workingTime.freeTime.asMinutes().toHM());
+                $rt.html(work.breakDuration.asMinutes().toHM());
                 $tt.html(totalTimeDuration.asMinutes().toHM());
 
                 $st.prop("disabled", false);
                 $et.prop("disabled", false);
-                $ft.prop("disabled", false);
+                $bd.prop("disabled", false);
             }
         };
 
         $("#test-modal-button").on('click', function () {
-            var workingTime = workingTimes[weekDay];
+            var work = workingsArr[pointer];
 
-            workingTimes[weekDay] = formToData();
+            formToWork(work);
 
             $('#test-modal').modal('hide');
 
-            exports.emit("WorkingTimeChanged", weekDay);
+            exports.emit("WorkingTimeChanged", pointer);
             exports.emit("WorkingTimesChanged");
         });
 
         $('#test-modal').on('show.bs.modal', function (event) {
-            weekDay = event.relatedTarget;
-            var workingTime = workingTimes[weekDay];
+            pointer = event.relatedTarget;
+            var working = workingsArr[pointer];
 
-            if (workingTime.isBreak) {
+            if (working.isBreak) {
                 $st.val("");
                 $et.val("");
-                $ft.val("");
+                $bd.val("");
                 $ib.prop("checked", true);
             } else {
                 // initial all value
-                $st.val(workingTime.startTime.format("HH:mm"));
-                $et.val(workingTime.endTime.format("HH:mm"));
-                $ft.val(workingTime.freeTime.asMinutes());
+                $st.val(working.startTime.format("HH:mm"));
+                $et.val(working.endTime.format("HH:mm"));
+                $bd.val(working.breakDuration.asMinutes());
                 $ib.prop("checked", false);
             }
-            $irdo.prop("checked", workingTime.isRoutineDayOff);
+            $irdo.prop("checked", working.isRoutineDayOff);
 
-            $("#myModalWeekDaySpan").html(["日", "一", "二", "三", "四", "五", "六"][weekDay]);
+            $("#myModalWeekDaySpan").html(["日", "一", "二", "三", "四", "五", "六"][pointer]);
 
             changeResult();
         });
     }
    
     // init the Table, run once
+    var $wct = $("#workingCollectionTable");
     function initTable() {
-        [1, 2, 3, 4, 5, 6, 7].forEach(function(item) {
-            var weekDay = item - 1;
-            var workingTime = workingTimes[weekDay];
+        workingsArr.forEach(function (working, i) {
+            var html = 
+                "<div class=\"row oneday\" id=\"weekday-" + i + "\">" + 
+                "<div class=\"col-md-12 week\">" +  ["日", "一", "二", "三", "四", "五", "六"][i] + "</div>" + 
+                "<div class=\"col-md-12 start\"></div>" +
+                "<div class=\"col-md-12 end\"></div>" +
+                "<div class=\"col-md-12 break\"></div>" +
+                "<div class=\"col-md-12 workingTime\"></div>" +
+                "</div>";
+            $(html)
+                .appendTo($wct)
+                .on('click', function () {
+                    $('#test-modal').modal('show', i);
+                });
+            
+            drawWorkingTime(i);
+        });
 
-            $("#weekday-" + item).on('click', function () {
-                $('#test-modal').modal('show', weekDay);
-            });
-
-            drawWorkingTime(weekDay);
+        exports.on("WorkingTimeChanged", function(i) {
+            drawWorkingTime(i);
         });
     }
 
-    exports.on("WorkingTimeChanged", function(weekDay) {
-        drawWorkingTime(weekDay);
-    });
 
-    function drawWorkingTime(weekDay) {
-        var workingTime = workingTimes[weekDay];
+    function drawWorkingTime(i) {
+        var working = workingsArr[i];
 
         // Update Table
-        var me = $("#weekday-" + (weekDay + 1));
+        var me = $("#weekday-" + i);
 
-        if (workingTime.isBreak) {
+        if (working.isBreak) {
             me.find(".start").html("");
             me.find(".end").html("");
             me.find(".break").html("");
         } else {
-            me.find(".start").html(workingTime.startTime.format("HH:mm"));
-            me.find(".end").html(workingTime.endTime.format("HH:mm"));
-            me.find(".break").html(workingTime.freeTime.asMinutes());
+            me.find(".start").html(working.startTime.format("HH:mm"));
+            me.find(".end").html(working.endTime.format("HH:mm"));
+            me.find(".break").html(working.breakDuration.asMinutes());
         }
-        if (workingTime.isRoutineDayOff) {
+        if (working.isRoutineDayOff) {
             me.css("background-color", "#FF3333");
         } else {
             me.css("background-color", "");
@@ -176,6 +189,7 @@
      * A place to prepare our checker
      */
     var init = exports.init = function(callback) {
+        initData();
         initTable();
         initModal();
 
@@ -192,8 +206,6 @@
 })(window.workingTimesForm = window.workingTimesForm || {}, jQuery, moment, window.calc);
 
 window.workingTimesForm.init(function () {
-    $("#weekday-1").click();
-
     workingTimesForm.on("WorkingTimesChanged", function() {
         update();
     });
@@ -205,14 +217,13 @@ window.workingTimesForm.init(function () {
     update();
 
     function update() {
-        var workingTimes = window.workingTimesForm.workingTimes;
+        var workingsArr = window.workingTimesForm.workingsArr;
         var monthSalary = window.workingTimesForm.monthSalary;
         var hourSalary = monthSalary / 240;
 
-        $.each(workingTimes, function(i, workingTime) {
-            var weekDay = i + 1;
-            $("#weekday-" + weekDay).find(".workingTime").text(
-                calc.calcWorkingTime(workingTime.startTime, workingTime.endTime, workingTime.freeTime).asHours()
+        $.each(workingsArr, function(i, working) {
+            $("#weekday-" + i).find(".workingTime").text(
+                working.isBreak ? 0 : calc.calcWorkingTime(working.startTime, working.endTime, working.breakDuration).asHours()
             );
         });
 
@@ -221,15 +232,15 @@ window.workingTimesForm.init(function () {
          */
         var startTimeArr = [], endTimeArr = [], breakDurationArr = [];
 
-        $.each(workingTimes, function(i, workingTime) {
-            if (workingTime.isBreak) {
+        $.each(workingsArr, function(i, working) {
+            if (working.isBreak) {
                 startTimeArr.push(null);
                 endTimeArr.push(null);
                 breakDurationArr.push(null);
             } else {
-                startTimeArr.push(workingTime.startTime);
-                endTimeArr.push(workingTime.endTime);
-                breakDurationArr.push(workingTime.freeTime);
+                startTimeArr.push(working.startTime);
+                endTimeArr.push(working.endTime);
+                breakDurationArr.push(working.freeTime);
             }
         });
 
@@ -237,4 +248,6 @@ window.workingTimesForm.init(function () {
         var overtimeSalary = calc.calcOvertimeSalary_OneWeek(startTimeArr, endTimeArr, breakDurationArr, hourSalary);
         console.log(overtimeSalary);
     }
+
+    $("#weekday-1").click();
 });
